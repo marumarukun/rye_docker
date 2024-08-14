9eaa28f8a130ecb2eb7554a59d0b7fa63a5f4bdd
Masato Ueda <cheriera.0310@gmail.com>
2024-08-14 22:55:00 +0900

    fix: 1階層上のディレクトリが無いパスも受け入れるよう修正

--- src/dir.py
+++ src/dir.py
@@ -5,5 +5,5 @@ def create_dir(path: str):
     if os.path.isdir(path):
         print(f"Directory already exists: {path}")
     else:
-        os.mkdir(path)
+        os.makedirs(path, exist_ok=True)
         print(f"Directory created: {path}")
